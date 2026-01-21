25f51f8b980eeca8f96bddcd83d7b90de66f0481
Automation8248 <automation.agent8248@gmail.com>
2026-01-21 14:12:06 +0530

    Update gmail_smtp.py

--- app/gmail_smtp.py
+++ app/gmail_smtp.py
@@ -1,1 +1,16 @@
+import os, smtplib
+from email.mime.text import MIMEText
 
+def send_email(to, subject, body):
+    user=os.getenv("EMAIL_USER")
+    pwd=os.getenv("EMAIL_PASS")
+    msg=MIMEText(body)
+    msg["From"]=user
+    msg["To"]=to
+    msg["Subject"]=subject
+    s=smtplib.SMTP("smtp.gmail.com",587)
+    s.starttls()
+    s.login(user,pwd)
+    s.send_message(msg)
+    s.quit()
+  
